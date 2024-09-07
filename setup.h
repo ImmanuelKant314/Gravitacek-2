cb24b0961d801758641a5523a6b26d016e586418
Karel Kraus <krauskarel0@gmail.com>
2024-09-07 14:28:05 +0200

    feat: add setup.h

---
+++ setup.h
@@ -0,0 +1,16 @@
+#ifndef __SETUP_H
+#define __SETUP_H
+
+// ==================== Types ==================== 
+
+typedef long double REAL;
+
+
+// ==================== Constants ==================== 
+
+const REAL pi   = 3.1415926535897932385;
+const REAL pi_2 = 1.5707963267948966192;
+const REAL pi_4 = 0.78539816339744830962;
+const REAL e    = 2.7182818284590452354;
+
+#endif 
