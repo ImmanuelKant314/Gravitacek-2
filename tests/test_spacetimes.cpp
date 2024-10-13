03a6af0b61d5b28d641ad5b56f2559b15cdefdb3
Karel Kraus <krauskarel0@gmail.com>
2024-10-13 06:34:32 +0200

    test: improve tests of spacetimes

--- tests/test_spacetimes.cpp
+++ tests/test_spacetimes.cpp
@@ -51,123 +51,118 @@ gr2::real riemann_tensor_weylschwarzschild[4][4][4][4] = {};
 
 TEST(WeylSchwarzschild, potencial)
 {
-    gr2::WeylSchwarzschild weylschwarzschild = gr2::WeylSchwarzschild(0.3);
-    weylschwarzschild.calculate_nu(y_weylschwarzschild);
-    EXPECT_NEAR(weylschwarzschild.get_nu(), -0.029413667983693322, eps);
+    gr2::WeylSchwarzschild spacetime = gr2::WeylSchwarzschild(0.3);
+    spacetime.calculate_nu(y_weylschwarzschild);
+    EXPECT_NEAR(spacetime.get_nu(), -0.029413667983693322, eps);
 }
 
 TEST(WeylSchwarzchild, DerivativesOfPotential)
 {
-    gr2::WeylSchwarzschild weylschwarzschild = gr2::WeylSchwarzschild(0.3);
-    weylschwarzschild.calculate_nu1(y_weylschwarzschild);
-    EXPECT_NEAR(weylschwarzschild.get_nu(), -0.029413667983693322, eps);
-    EXPECT_NEAR(weylschwarzschild.get_nu_rho(), 0.0028276099493778893, eps);
-    EXPECT_NEAR(weylschwarzschild.get_nu_z(), 0.0005650330027458911, eps);
+    gr2::WeylSchwarzschild spacetime = gr2::WeylSchwarzschild(0.3);
+    spacetime.calculate_nu1(y_weylschwarzschild);
+    EXPECT_NEAR(spacetime.get_nu(), -0.029413667983693322, eps);
+    EXPECT_NEAR(spacetime.get_nu_rho(), 0.0028276099493778893, eps);
+    EXPECT_NEAR(spacetime.get_nu_z(), 0.0005650330027458911, eps);
 }
 
 TEST(WeylSchwarzschild, Lambda)
 {
-    gr2::WeylSchwarzschild weylschwarzschild = gr2::WeylSchwarzschild(0.3);
-    weylschwarzschild.calculate_lambda(y_weylschwarzschild);
-    EXPECT_NEAR(weylschwarzschild.get_lambda(), -0.000415904944973, eps);
+    gr2::WeylSchwarzschild spacetime = gr2::WeylSchwarzschild(0.3);
+    spacetime.calculate_lambda(y_weylschwarzschild);
+    EXPECT_NEAR(spacetime.get_lambda(), -0.000415904944973, eps);
 }
 
 TEST(WeylSchwarzschild, Metric)
 {
-    gr2::WeylSchwarzschild weylschwarzschild = gr2::WeylSchwarzschild(0.3);
-    weylschwarzschild.calculate_metric(y_weylschwarzschild);
+    gr2::WeylSchwarzschild spacetime = gr2::WeylSchwarzschild(0.3);
+    spacetime.calculate_metric(y_weylschwarzschild);
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
-            EXPECT_NEAR(weylschwarzschild.get_metric()[i][j], metric_weylschwarzschild[i][j], eps);
+            EXPECT_NEAR(spacetime.get_metric()[i][j], metric_weylschwarzschild[i][j], eps);
 }
 
 TEST(WeylSchwarzschild, ChristoffelSymbols)
 {
-    gr2::WeylSchwarzschild weylschwarzschild = gr2::WeylSchwarzschild(0.3);
-    weylschwarzschild.calculate_christoffel_symbols(y_weylschwarzschild);
+    gr2::WeylSchwarzschild spacetime = gr2::WeylSchwarzschild(0.3);
+    spacetime.calculate_christoffel_symbols(y_weylschwarzschild);
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
             for(int k = 0; k < 4; k++)
-                EXPECT_NEAR(weylschwarzschild.get_christoffel_symbols()[i][j][k], christoffel_symbols_weylschwarzschild[i][j][k], 4*eps);
+                EXPECT_NEAR(spacetime.get_christoffel_symbols()[i][j][k], christoffel_symbols_weylschwarzschild[i][j][k], 4*eps);
 }
 
 //TODO: WeylSchwarzschild - Riemannův tensor
 
 int main(int argc, char **argv)
 {
-    // ========== prepare schwarzschild ========== 
-    std::ifstream file("data/schwarzschild.txt");
-
-    // vector
-    while (getline(file,line))
-    {
-        if (line == "")
-            break;
-        if (line[0] == '#')
-            continue;
-
-        int i = std::stoi(line.substr(0, 1));
-        gr2::real value = std::stold(line.substr(2));
-        y_schwarzschild[i] = value;
-    }
+    // ========== Prepare schwarzschild ========== 
+    // coordinates
+    y_schwarzschild[0] = 1.000000000000000;
+    y_schwarzschild[1] = 4.000000000000000;
+    y_schwarzschild[2] = 0.500000000000000;
+    y_schwarzschild[3] = 0.300000000000000;
 
     // metric
-    while (getline(file,line))
-    {
-        if (line == "")
-            break;
-        if (line[0] == '#')
-            continue;
-
-        int i = std::stoi(line.substr(0, 1));
-        int j = std::stoi(line.substr(2, 1));
-        gr2::real value = std::stold(line.substr(4));
-        metric_schwarzschild[i][j] = value;
-    }
+    metric_schwarzschild[0][0] = -0.850000000000000;
+    metric_schwarzschild[1][1] = 1.176470588235294;
+    metric_schwarzschild[2][2] = 16.000000000000000;
+    metric_schwarzschild[3][3] = 3.677581553054882;
 
     // christoffel symbols
-    while (getline(file,line))
-    {
-        if (line == "")
-            break;
-        if (line[0] == '#')
-            continue;
-
-        int i = std::stoi(line.substr(0, 1));
-        int j = std::stoi(line.substr(2, 1));
-        int k = std::stoi(line.substr(4, 1));
-        gr2::real value = std::stold(line.substr(6));
-        christoffel_symbols_schwarzschild[i][j][k] = value;
-    }
+    christoffel_symbols_schwarzschild[0][0][1] = 0.022058823529412;
+    christoffel_symbols_schwarzschild[0][1][0] = 0.022058823529412;
+    christoffel_symbols_schwarzschild[1][0][0] = 0.015937500000000;
+    christoffel_symbols_schwarzschild[1][1][1] = -0.022058823529412;
+    christoffel_symbols_schwarzschild[1][2][2] = -3.400000000000000;
+    christoffel_symbols_schwarzschild[1][3][3] = -0.781486080024162;
+    christoffel_symbols_schwarzschild[2][1][2] = 0.250000000000000;
+    christoffel_symbols_schwarzschild[2][2][1] = 0.250000000000000;
+    christoffel_symbols_schwarzschild[2][3][3] = -0.420735492403948;
+    christoffel_symbols_schwarzschild[3][1][3] = 0.250000000000000;
+    christoffel_symbols_schwarzschild[3][2][3] = 1.830487721712452;
+    christoffel_symbols_schwarzschild[3][3][1] = 0.250000000000000;
+    christoffel_symbols_schwarzschild[3][3][2] = 1.830487721712452;
 
     // riemanns tensor
-    while (getline(file,line))
-    {
-        if (line == "")
-            break;
-        if (line[0] == '#')
-            continue;
+    riemann_tensor_schwarzschild[0][1][0][1] = 0.011029411764706;
+    riemann_tensor_schwarzschild[0][1][1][0] = -0.011029411764706;
+    riemann_tensor_schwarzschild[0][2][0][2] = -0.075000000000000;
+    riemann_tensor_schwarzschild[0][2][2][0] = 0.075000000000000;
+    riemann_tensor_schwarzschild[0][3][0][3] = -0.017238663529945;
+    riemann_tensor_schwarzschild[0][3][3][0] = 0.017238663529945;
+    riemann_tensor_schwarzschild[1][0][0][1] = 0.007968750000000;
+    riemann_tensor_schwarzschild[1][0][1][0] = -0.007968750000000;
+    riemann_tensor_schwarzschild[1][2][1][2] = -0.075000000000000;
+    riemann_tensor_schwarzschild[1][2][2][1] = 0.075000000000000;
+    riemann_tensor_schwarzschild[1][3][1][3] = -0.017238663529945;
+    riemann_tensor_schwarzschild[1][3][3][1] = 0.017238663529945;
+    riemann_tensor_schwarzschild[2][0][0][2] = -0.003984375000000;
+    riemann_tensor_schwarzschild[2][0][2][0] = 0.003984375000000;
+    riemann_tensor_schwarzschild[2][1][1][2] = 0.005514705882353;
+    riemann_tensor_schwarzschild[2][1][2][1] = -0.005514705882353;
+    riemann_tensor_schwarzschild[2][3][2][3] = 0.034477327059890;
+    riemann_tensor_schwarzschild[2][3][3][2] = -0.034477327059890;
+    riemann_tensor_schwarzschild[3][0][0][3] = -0.003984375000000;
+    riemann_tensor_schwarzschild[3][0][3][0] = 0.003984375000000;
+    riemann_tensor_schwarzschild[3][1][1][3] = 0.005514705882353;
+    riemann_tensor_schwarzschild[3][1][3][1] = -0.005514705882353;
+    riemann_tensor_schwarzschild[3][2][2][3] = -0.150000000000000;
+    riemann_tensor_schwarzschild[3][2][3][2] = 0.150000000000000;
 
-        int i = std::stoi(line.substr(0, 1));
-        int j = std::stoi(line.substr(2, 1));
-        int k = std::stoi(line.substr(4, 1));
-        int l = std::stoi(line.substr(6, 1));
-        gr2::real value = std::stold(line.substr(8));
-        riemann_tensor_schwarzschild[i][j][k][l] = value;
-    }
-    
-    file.close();
-
+    // ========== Prepare WeylSchwarzschild ========== 
+    // coordinates
     y_weylschwarzschild[0] = 1.000000000000000;
     y_weylschwarzschild[1] = 3.000000000000000;
     y_weylschwarzschild[2] = 10.000000000000000;
     y_weylschwarzschild[3] = 2.000000000000000;
 
+    // metric
     metric_weylschwarzschild[0][0] = -0.942869554762639;
     metric_weylschwarzschild[1][1] = 106.059209882086250;
     metric_weylschwarzschild[2][2] = 1.059710254638068;
     metric_weylschwarzschild[3][3] = 1.059710254638068;
 
+    // christoffel symbols
     christoffel_symbols_weylschwarzschild[0][0][2] = 0.002827609949378;
     christoffel_symbols_weylschwarzschild[0][0][3] = 0.000565033002746;
     christoffel_symbols_weylschwarzschild[0][2][0] = 0.002827609949378;
