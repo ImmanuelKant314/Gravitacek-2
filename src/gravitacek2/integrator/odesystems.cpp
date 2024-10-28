af6428643a6edf703fc5d29790447af1acee634a
Karel Kraus <krauskarel0@gmail.com>
2024-10-28 22:09:10 +0100

    feat: add DampedHarmonicOscillator to namespace gr2

--- src/gravitacek2/integrator/odesystems.cpp
+++ src/gravitacek2/integrator/odesystems.cpp
@@ -1,12 +1,12 @@
 #include "gravitacek2/integrator/odesystems.hpp"
 
-DampedHarmonicOscillator::DampedHarmonicOscillator(const gr2::real &omega0, const gr2::real &xi):gr2::OdeSystem(2) 
+gr2::DampedHarmonicOscillator::DampedHarmonicOscillator(const gr2::real &omega0, const gr2::real &xi):gr2::OdeSystem(2) 
 {
     this->omega0 = omega0;
     this->xi = xi;
 };
 
-void DampedHarmonicOscillator::function(const gr2::real &t, const gr2::real y[], gr2::real dydt[])
+void gr2::DampedHarmonicOscillator::function(const gr2::real &t, const gr2::real y[], gr2::real dydt[])
 {
     dydt[0] = y[1];
     dydt[1] = -2*this->xi*y[1] - this->omega0*this->omega0*y[0];
