d17822561323f3dff4c61aa04bb21ba265737723
Karel Kraus <krauskarel0@gmail.com>
2025-02-17 21:43:14 +0100

    test: add parametric tests of spacetimes

--- tests/test_spacetimes.cpp
+++ tests/test_spacetimes.cpp
@@ -3,53 +3,164 @@
 
 #include "gtest/gtest.h"
 #include "gravitacek2/setup.hpp"
+#include "gravitacek2/geomotion/geomotion.hpp"
 #include "gravitacek2/geomotion/spacetimes.hpp"
 
-// ==================== Schwarzschild ==================== 
-
-gr2::real y_schwarzschild[4] = {};
-gr2::real metric_schwarzschild[4][4] = {};
-gr2::real christoffel_symbols_schwarzschild[4][4][4] = {};
-gr2::real riemann_tensor_schwarzschild[4][4][4][4] = {};
-gr2::real eps = 1e-15;
-std::string line;
-
-TEST(Schwarzschild, Metric)
+class SpacetimeTestCase
 {
-    gr2::Schwarzschild spacetime = gr2::Schwarzschild(0.3);
-    spacetime.calculate_metric(y_schwarzschild);
-    for (int i = 0; i < 4; i++)
+public:
+    std::shared_ptr<gr2::GeoMotion> spacetime;
+    gr2::real y[4];
+    gr2::real metric[4][4];
+    gr2::real christoffel_symbols[4][4][4];
+    gr2::real riemann_tensor[4][4][4][4];
+
+    gr2::real eps;
+
+    std::string name;
+    SpacetimeTestCase(std::shared_ptr<gr2::GeoMotion> spacetime, std::string filename, std::string name, gr2::real eps):
+    spacetime(spacetime), eps(eps), name(name), metric(), christoffel_symbols(), riemann_tensor()
+    {
+        std::ifstream file;
+        file.open(filename);
+
+        if (!file.is_open())
+            throw std::runtime_error("Unable to open file " + filename);
+
+        std::string line;
+        int c;
+        int i, j, k, l;
+        gr2::real value;
+
+        // position
+        std::getline(file, line);
+        c = sscanf(line.c_str(), "%Lf;%Lf;%Lf;%Lf%*s", y, y+1, y+2, y+3);
+        if (c != 4)
+            throw std::runtime_error("Unable to read position from file " + filename);
+
+        std::getline(file, line); // read line containing =====
+
+        // metric
+        while(true)
+        {
+            std::getline(file, line);
+            if (line[0] == '=')
+                break;
+            if (file.eof())
+                throw std::runtime_error("File " + filename + "does not contain all information for the test");
+            c = sscanf(line.c_str(), "%d;%d;%Lf%*s", &i, &j, &value);
+            if (c != 3)
+                throw std::runtime_error("Unable to read metric component from file " + filename);
+            this->metric[i][j] = value;
+        }
+
+        // Christoffel symbols
+        while(true)
+        {
+            std::getline(file, line);
+            if (line[0] == '=')
+                break;
+            if (file.eof())
+                throw std::runtime_error("File " + filename + "does not contain all information for the test");
+            c = sscanf(line.c_str(), "%d;%d;%d;%Lf%*s", &i, &j, &k, &value);
+            if (c != 4)
+                throw std::runtime_error("Unable to read Christoffel symbol component from file " + filename);
+            this->christoffel_symbols[i][j][k] = value;
+        }
+
+        // Riemann tensor
+        while(true)
+        {
+            std::getline(file, line);
+            if (line[0] == '=')
+                break;
+            c = sscanf(line.c_str(), "%d;%d;%d;%d;%Lf%*s", &i, &j, &k, &l, &value);
+            if (c != 5)
+                throw std::runtime_error("Unable to read Christoffel symbol component from file " + filename);
+            this->riemann_tensor[i][j][k][l] = value;
+            if (file.eof())
+                break;
+
+        }
+        file.close();
+    }
+};
+
+class GeneralSpacetimeTest :
+    public testing::TestWithParam<SpacetimeTestCase>
+{
+
+};
+
+TEST_P(GeneralSpacetimeTest, Metric)
+{
+    std::shared_ptr<gr2::GeoMotion> spacetime = GetParam().spacetime;
+    gr2::real eps = GetParam().eps;
+    spacetime->calculate_metric(GetParam().y);
+    for (int i = 0; i <4; i++)
         for (int j = 0; j < 4; j++)
-            EXPECT_NEAR(spacetime.get_metric()[i][j], metric_schwarzschild[i][j], eps);
+        {
+            gr2::real value = GetParam().metric[i][j];
+            EXPECT_NEAR(spacetime->get_metric()[i][j], value, eps + std::llabs(value)*eps);
+        }
 }
 
-TEST(Schwarzschild, ChristoffelSymbols)
+TEST_P(GeneralSpacetimeTest, ChristoffelSymbols)
 {
-    gr2::Schwarzschild spacetime = gr2::Schwarzschild(0.3);
-    spacetime.calculate_christoffel_symbols(y_schwarzschild);
-    for (int i = 0; i < 4; i++)
+    std::shared_ptr<gr2::GeoMotion> spacetime = GetParam().spacetime;
+    gr2::real eps = GetParam().eps;
+    spacetime->calculate_christoffel_symbols(GetParam().y);
+    for (int i = 0; i <4; i++)
         for (int j = 0; j < 4; j++)
-            for(int k = 0; k < 4; k++)
-                EXPECT_NEAR(spacetime.get_christoffel_symbols()[i][j][k], christoffel_symbols_schwarzschild[i][j][k], eps);
+            for (int k = 0; k < 4; k++)
+            {
+                gr2::real value = GetParam().christoffel_symbols[i][j][k];
+                EXPECT_NEAR(spacetime->get_christoffel_symbols()[i][j][k], value, eps + std::llabs(value)*eps);
+            }
 }
 
-TEST(Schwarzschild, RiemannTensor)
+TEST_P(GeneralSpacetimeTest, RiemannTensor)
 {
-    gr2::Schwarzschild spacetime = gr2::Schwarzschild(0.3);
-    spacetime.calculate_riemann_tensor(y_schwarzschild);
-    for (int i = 0; i < 4; i++)
+    std::shared_ptr<gr2::GeoMotion> spacetime = GetParam().spacetime;
+    gr2::real eps = GetParam().eps;
+    spacetime->calculate_riemann_tensor(GetParam().y);
+    for (int i = 0; i <4; i++)
         for (int j = 0; j < 4; j++)
             for (int k = 0; k < 4; k++)
                 for (int l = 0; l < 4; l++)
-                    EXPECT_NEAR(spacetime.get_riemann_tensor()[i][j][k][l], riemann_tensor_schwarzschild[i][j][k][l], eps);
+                {
+                    gr2::real value = GetParam().riemann_tensor[i][j][k][l];
+                    EXPECT_NEAR(spacetime->get_riemann_tensor()[i][j][k][l], value, eps + std::llabs(value)*eps);
+                }
 }
+
+void PrintTo(const SpacetimeTestCase& testcase, std::ostream* os) {
+    *os << "0";
+}
+
+std::string MyTestNameGenerator(const ::testing::TestParamInfo<SpacetimeTestCase>& info) {
+    return info.param.name;
+}
+
+std::string folder = "./test_spacetime/";
+
+auto test_cases = testing::Values(
+    SpacetimeTestCase(std::make_shared<gr2::Schwarzschild>(0.3), folder + "schwarzschild.txt", "Schwarzschild", 1e-15),
+    SpacetimeTestCase(std::make_shared<gr2::WeylSchwarzschild>(0.3, gr2::exact, gr2::exact), folder + "weyl.txt", "Weyl", 1e-14)
+);
+
+INSTANTIATE_TEST_SUITE_P(
+    SpacetimeTest,
+    GeneralSpacetimeTest,
+    test_cases,
+    MyTestNameGenerator
+);
 
 // ==================== WeylSchwarzschild ==================== 
 
 gr2::real y_weylschwarzschild[4] = {};
 gr2::real metric_weylschwarzschild[4][4] = {};
-gr2::real christoffel_symbols_weylschwarzschild[4][4][4] = {};
-gr2::real riemann_tensor_weylschwarzschild[4][4][4][4] = {};
+gr2::real eps = 1e-10;
 
 TEST(WeylSchwarzschild, LambdaExact)
 {
@@ -65,117 +176,14 @@ TEST(WeylSchwarzschild, LambdaIntegral)
     EXPECT_NEAR(spacetime.get_lambda(), -0.000415904944973, eps);
 }
 
-TEST(WeylSchwarzschild, Metric)
-{
-    gr2::WeylSchwarzschild spacetime = gr2::WeylSchwarzschild(0.3,gr2::exact,gr2::exact);
-    spacetime.calculate_metric(y_weylschwarzschild);
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            EXPECT_NEAR(spacetime.get_metric()[i][j], metric_weylschwarzschild[i][j], eps);
-}
-
-TEST(WeylSchwarzschild, ChristoffelSymbols)
-{
-    gr2::WeylSchwarzschild spacetime = gr2::WeylSchwarzschild(0.3,gr2::exact, gr2::exact);
-    spacetime.calculate_christoffel_symbols(y_weylschwarzschild);
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            for(int k = 0; k < 4; k++)
-                EXPECT_NEAR(spacetime.get_christoffel_symbols()[i][j][k], christoffel_symbols_weylschwarzschild[i][j][k], 4*eps);
-}
-
-//TODO: WeylSchwarzschild - Riemannův tensor
-
 int main(int argc, char **argv)
 {
-    // ========== Prepare schwarzschild ========== 
-    // coordinates
-    y_schwarzschild[0] = 1.000000000000000;
-    y_schwarzschild[1] = 4.000000000000000;
-    y_schwarzschild[2] = 0.500000000000000;
-    y_schwarzschild[3] = 0.300000000000000;
-
-    // metric
-    metric_schwarzschild[0][0] = -0.850000000000000;
-    metric_schwarzschild[1][1] = 1.176470588235294;
-    metric_schwarzschild[2][2] = 16.000000000000000;
-    metric_schwarzschild[3][3] = 3.677581553054882;
-
-    // christoffel symbols
-    christoffel_symbols_schwarzschild[0][0][1] = 0.022058823529412;
-    christoffel_symbols_schwarzschild[0][1][0] = 0.022058823529412;
-    christoffel_symbols_schwarzschild[1][0][0] = 0.015937500000000;
-    christoffel_symbols_schwarzschild[1][1][1] = -0.022058823529412;
-    christoffel_symbols_schwarzschild[1][2][2] = -3.400000000000000;
-    christoffel_symbols_schwarzschild[1][3][3] = -0.781486080024162;
-    christoffel_symbols_schwarzschild[2][1][2] = 0.250000000000000;
-    christoffel_symbols_schwarzschild[2][2][1] = 0.250000000000000;
-    christoffel_symbols_schwarzschild[2][3][3] = -0.420735492403948;
-    christoffel_symbols_schwarzschild[3][1][3] = 0.250000000000000;
-    christoffel_symbols_schwarzschild[3][2][3] = 1.830487721712452;
-    christoffel_symbols_schwarzschild[3][3][1] = 0.250000000000000;
-    christoffel_symbols_schwarzschild[3][3][2] = 1.830487721712452;
-
-    // riemanns tensor
-    riemann_tensor_schwarzschild[0][1][0][1] = 0.011029411764706;
-    riemann_tensor_schwarzschild[0][1][1][0] = -0.011029411764706;
-    riemann_tensor_schwarzschild[0][2][0][2] = -0.075000000000000;
-    riemann_tensor_schwarzschild[0][2][2][0] = 0.075000000000000;
-    riemann_tensor_schwarzschild[0][3][0][3] = -0.017238663529945;
-    riemann_tensor_schwarzschild[0][3][3][0] = 0.017238663529945;
-    riemann_tensor_schwarzschild[1][0][0][1] = 0.007968750000000;
-    riemann_tensor_schwarzschild[1][0][1][0] = -0.007968750000000;
-    riemann_tensor_schwarzschild[1][2][1][2] = -0.075000000000000;
-    riemann_tensor_schwarzschild[1][2][2][1] = 0.075000000000000;
-    riemann_tensor_schwarzschild[1][3][1][3] = -0.017238663529945;
-    riemann_tensor_schwarzschild[1][3][3][1] = 0.017238663529945;
-    riemann_tensor_schwarzschild[2][0][0][2] = -0.003984375000000;
-    riemann_tensor_schwarzschild[2][0][2][0] = 0.003984375000000;
-    riemann_tensor_schwarzschild[2][1][1][2] = 0.005514705882353;
-    riemann_tensor_schwarzschild[2][1][2][1] = -0.005514705882353;
-    riemann_tensor_schwarzschild[2][3][2][3] = 0.034477327059890;
-    riemann_tensor_schwarzschild[2][3][3][2] = -0.034477327059890;
-    riemann_tensor_schwarzschild[3][0][0][3] = -0.003984375000000;
-    riemann_tensor_schwarzschild[3][0][3][0] = 0.003984375000000;
-    riemann_tensor_schwarzschild[3][1][1][3] = 0.005514705882353;
-    riemann_tensor_schwarzschild[3][1][3][1] = -0.005514705882353;
-    riemann_tensor_schwarzschild[3][2][2][3] = -0.150000000000000;
-    riemann_tensor_schwarzschild[3][2][3][2] = 0.150000000000000;
-
     // ========== Prepare WeylSchwarzschild ========== 
     // coordinates
     y_weylschwarzschild[0] = 0.000000000000000;
     y_weylschwarzschild[1] = 0.000000000000000;
     y_weylschwarzschild[2] = 10.000000000000000;
     y_weylschwarzschild[3] = 2.000000000000000;
-
-    // metric
-    metric_weylschwarzschild[0][0] = -0.942869554762639;
-    metric_weylschwarzschild[1][1] = 106.059209882086250;
-    metric_weylschwarzschild[2][2] = 1.059710254638068;
-    metric_weylschwarzschild[3][3] = 1.059710254638068;
-
-    // christoffel symbols
-    christoffel_symbols_weylschwarzschild[0][0][2] = 0.002827609949378;
-    christoffel_symbols_weylschwarzschild[0][0][3] = 0.000565033002746;
-    christoffel_symbols_weylschwarzschild[0][2][0] = 0.002827609949378;
-    christoffel_symbols_weylschwarzschild[0][3][0] = 0.000565033002746;
-    christoffel_symbols_weylschwarzschild[1][1][2] = 0.097172390050622;
-    christoffel_symbols_weylschwarzschild[1][1][3] = -0.000565033002746;
-    christoffel_symbols_weylschwarzschild[1][2][1] = 0.097172390050622;
-    christoffel_symbols_weylschwarzschild[1][3][1] = -0.000565033002746;
-    christoffel_symbols_weylschwarzschild[2][0][0] = 0.002515845555277;
-    christoffel_symbols_weylschwarzschild[2][1][1] = -9.725325263218092;
-    christoffel_symbols_weylschwarzschild[2][2][2] = -0.002750848792062;
-    christoffel_symbols_weylschwarzschild[2][2][3] = -0.000533079143940;
-    christoffel_symbols_weylschwarzschild[2][3][2] = -0.000533079143940;
-    christoffel_symbols_weylschwarzschild[2][3][3] = 0.002750848792062;
-    christoffel_symbols_weylschwarzschild[3][0][0] = 0.000502734038284;
-    christoffel_symbols_weylschwarzschild[3][1][1] = 0.056550319831527;
-    christoffel_symbols_weylschwarzschild[3][2][2] = 0.000533079143940;
-    christoffel_symbols_weylschwarzschild[3][2][3] = -0.002750848792062;
-    christoffel_symbols_weylschwarzschild[3][3][2] = -0.002750848792062;
-    christoffel_symbols_weylschwarzschild[3][3][3] = -0.000533079143940;
 
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
