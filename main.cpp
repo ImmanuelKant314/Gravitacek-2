0211e8992db2deac76142198c8efe30686c66a8a
Karel Kraus <krauskarel0@gmail.com>
2025-03-03 07:13:00 +0100

    feat: measure time

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+
 #include "gravitacek2/setup.hpp"
 #include "gravitacek2/integrator/odesystem.hpp"
 #include "interface/interface.hpp"
@@ -14,8 +16,11 @@ int main()
     {
         std::getline(std::cin, text);
         try
-        {
+        { 
+            auto start = std::chrono::high_resolution_clock::now();
             test = intf.command(text);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::cout << "Time of execution: " << std::chrono::duration_cast<std::chrono::duration<double>>(end-start).count() << " s" << std::endl;
         }
         catch(const std::exception& e)
         {
